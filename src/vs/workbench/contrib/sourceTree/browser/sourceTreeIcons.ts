db357beea9e4e743cf63c756a665f47c1a1e2109
tanomitsu2002 <tanomitsu2002@g.ecc.u-tokyo.ac.jp>
2022-10-23 19:21:55 +0900

    feat: サイドバーにタブを追加

--- src/vs/workbench/contrib/sourceTree/browser/sourceTreeIcons.ts
+++ src/vs/workbench/contrib/sourceTree/browser/sourceTreeIcons.ts
@@ -1,4 +1,7 @@
-/* eslint-disable header/header */
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import { Codicon } from 'vs/base/common/codicons';
 
 export const sourceTreeIcon = Codicon.listTree;
